d1654a9c57c576a319906419fe59c172f5761dd0
Cale Lamb <calejlamb@gmail.com>
2026-04-18 16:01:23 -0600

    feat(tokens): load tokens.css before index.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { loader } from '@monaco-editor/react'
 import * as monaco from 'monaco-editor'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import './styles/tokens.css'
 import './index.css'
 import App from './App.tsx'
 
